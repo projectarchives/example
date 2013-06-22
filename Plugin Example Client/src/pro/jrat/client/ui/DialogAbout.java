d597302af5fcab3f9f905d1039d5db6c6d98e1bc
redpois0n <red@cmail.nu>
2013-06-22 15:14:52 +0200

    About Dialog

---
+++ Plugin Example Client/src/pro/jrat/client/ui/DialogAbout.java
@@ -0,0 +1,13 @@
+package pro.jrat.client.ui;
+
+import javax.swing.JDialog;
+
+@SuppressWarnings("serial")
+public class DialogAbout extends JDialog {
+
+	public DialogAbout() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
