9cb03e0ede1a7d5725a4945728e90914aedc93e8
redpois0n <red@cmail.nu>
2013-06-22 15:23:58 +0200

    Example control panel panel (yes)

---
+++ Plugin Example Client/src/pro/jrat/client/ui/ExampleControlPanel.java
@@ -0,0 +1,12 @@
+package pro.jrat.client.ui;
+
+import pro.jrat.api.BaseControlPanel;
+
+@SuppressWarnings("serial")
+public class ExampleControlPanel extends BaseControlPanel {
+
+	public ExampleControlPanel() {
+
+	}
+
+}
